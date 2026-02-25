bf90d1fead7c2c71a4c4c72807b776bb58214516
galimsarov <galimsarov@pss.ru>
2026-02-25 15:38:35 +0500

    Добавил примеров

---
+++ src/main/java/com/example/b_advanced_core/b_iterable/BIteratorRemoveDemo.java
@@ -0,0 +1,21 @@
+package com.example.b_advanced_core.b_iterable;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.List;
+
+public class BIteratorRemoveDemo {
+    public static void main(String[] args) {
+        List<Integer> nums = new ArrayList<>(List.of(1, 2, 3, 4, 5, 6));
+        Iterator<Integer> it = nums.iterator();
+
+        while (it.hasNext()) {
+            Integer n = it.next();
+            if (n % 2 == 0) {
+                it.remove(); // безопасно удаляем текущий элемент
+            }
+        }
+
+        System.out.println(nums); // [1, 3, 5]
+    }
+}
