1fcd135d2764bf6350c89c9b5596115d3751e1b3
galimsarov <galimsarov@pss.ru>
2026-02-27 14:38:02 +0500

    I/O

---
+++ src/main/java/com/example/b_advanced_core/g_input_output/ABasicStreamDemo.java
@@ -0,0 +1,73 @@
+package com.example.b_advanced_core.g_input_output;
+
+import java.io.*;
+import java.nio.charset.StandardCharsets;
+
+/**
+ * В классическом java.io есть 2 базовые иерархии:
+ * - InputStream/OutputStream — работа с байтами.
+ * - Reader/Writer — работа с символами (текст, кодировки).
+ */
+public class ABasicStreamDemo {
+    public static void main(String[] args) throws Exception {
+        inputOutputStreamDemo();
+        readerWriterDemo();
+    }
+
+    /**
+     * Это низкоуровневая работа с байтами.
+     * Использовать, если:
+     * - читаешь/пишешь файлы любого типа
+     * - работаешь с изображениями
+     * - передаёшь PDF
+     * - читаешь zip
+     * - работаешь с сетью (сокеты)
+     * - сериализация
+     * - бинарные протоколы
+     * Здесь данные — просто последовательность байтов. JVM не пытается их интерпретировать.
+     */
+    private static void inputOutputStreamDemo() throws IOException {
+        // Байт-уровень: в памяти
+        byte[] src = "Hello IO".getBytes(StandardCharsets.UTF_8);
+        try (InputStream in = new ByteArrayInputStream(src);
+             OutputStream out = new ByteArrayOutputStream()) {
+
+            byte[] buffer = new byte[4];
+            int read;
+            while ((read = in.read(buffer)) != -1) {
+                out.write(buffer, 0, read);
+            }
+
+            String result = out.toString();
+            System.out.println(result); // Hello IO
+        }
+    }
+
+    /**
+     * Это работа с текстом.
+     * Использовать, если:
+     * - читаешь .txt
+     * - работаешь с JSON
+     * - читаешь XML
+     * - читаешь CSV
+     * - лог-файлы
+     * - конфигурации
+     * Здесь уже происходит:
+     * 👉 преобразование байтов в символы
+     * 👉 учёт кодировки
+     */
+    private static void readerWriterDemo() throws IOException {
+        // Символьный уровень: Reader/Writer
+        try (Reader reader = new StringReader("Привет, Reader/Writer");
+             StringWriter writer = new StringWriter()) {
+
+            char[] cbuf = new char[8];
+            int read;
+            while ((read = reader.read(cbuf)) != -1) {
+                writer.write(cbuf, 0, read);
+            }
+
+            System.out.println(writer.toString());
+        }
+    }
+}
